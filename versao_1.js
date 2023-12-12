a8dfc032259fa58032872b938b3e496e643bbc18
bruhoffman <bruhoffman@gmail.com>
2023-12-12 15:21:43 -0300

    Blackjack versão 1.0

--- versao_1.js
+++ versao_1.js
@@ -1,12 +1,31 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.log("Boas vindas ao jogo de BlackJack!")
+
+if (confirm("Quer iniciar uma nova rodada?")) {
+   let cartasUsuario = [];
+   let cartasComputador = [];
+
+   for (let i = 1; i <= 2; i++) {
+      const cartaUsuario = comprarCarta();
+      cartasUsuario.push(cartaUsuario);
+   }
+
+   for (let i = 1; i <= 2; i++) {
+      const cartaComputador = comprarCarta();
+      cartasComputador.push(cartaComputador);
+   }
+
+   const somaCartasUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor;
+   const somaCartasComputador = cartasComputador[0].valor + cartasComputador[1].valor;
+
+   if (somaCartasUsuario > somaCartasComputador) {
+      console.log(`Usuário - cartas: ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} - pontuação: ${somaCartasUsuario}`);
+      console.log(`Computador - cartas: ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação: ${somaCartasComputador}`);
+      console.log(`O usuário ganhou!`);
+   } else {
+      console.log(`Usuário - cartas: ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} - pontuação: ${somaCartasUsuario}`);
+      console.log(`Computador - cartas: ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação: ${somaCartasComputador}`);
+      console.log(`O computador ganhou!`);
+   }
+} else {
+   console.log("O jogo acabou!")
+}
